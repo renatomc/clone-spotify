6a5a426544fb1147a3782896dbdac669b0b2bbfe
Renato Martinez Costa <renatomc@hotmail.com>
2023-05-09 09:19:45 -0300

    Album mouse affect and foter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react';
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react';
 import Image from 'next/image';
 
 export default function Home() {
@@ -44,39 +44,117 @@ export default function Home() {
          </div>
          <h1 className='font-semibold text-3xl mt-10'>Bom dia, Renato</h1>
          <div className='grid grid-cols-3 gap-4 mt-4'>
-           <a href='' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+           <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
              <Image src="/images/image.jpeg" width={104} height={104} alt="Capa do álbum" />
              <strong>Wasting Light</strong>
-             </a>
-           <a href='' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+               <Play />
+             </button>
+           </a>
+           <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
              <Image src="/images/image.jpeg" width={104} height={104} alt="Capa do álbum" />
              <strong>Wasting Light</strong>
-             </a>
-           <a href='' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+               <Play />
+             </button>
+           </a>
+           <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
              <Image src="/images/image.jpeg" width={104} height={104} alt="Capa do álbum" />
              <strong>Wasting Light</strong>
-             </a>
-           <a href='' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+               <Play />
+             </button>
+           </a>
+           <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
              <Image src="/images/image.jpeg" width={104} height={104} alt="Capa do álbum" />
              <strong>Wasting Light</strong>
-             </a>
-           <a href='' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+               <Play />
+             </button>
+           </a>
+           <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
              <Image src="/images/image.jpeg" width={104} height={104} alt="Capa do álbum" />
              <strong>Wasting Light</strong>
-             </a>
-           <a href='' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+               <Play />
+             </button>
+           </a>
+           <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
              <Image src="/images/image.jpeg" width={104} height={104} alt="Capa do álbum" />
              <strong>Wasting Light</strong>
-             </a>
-             <a href='' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-             <Image src="/images/image.jpeg" width={104} height={104} alt="Capa do álbum" />
-             <strong>Wasting Light</strong>
-             </a>
+             <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+               <Play />
+             </button>
+           </a>
+         </div>
+
+         <h2 className='font-semibold text-2xl mt-10'>Made for Renato Costa</h2>
+         <div className='grid grid-cols-8 gap-4 mt-4'>
+           <a href='' className='bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2'>
+            <Image className='w-full' src="/images/image.jpeg" width={240} height={240} alt="Capa do álbum" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-400'>Wallows, Coin, girl in red and more</span>
+           </a>
+           <a href='' className='bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2'>
+            <Image className='w-full' src="/images/image.jpeg" width={240} height={240} alt="Capa do álbum" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-400'>Wallows, Coin, girl in red and more</span>
+           </a>
+           <a href='' className='bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2'>
+            <Image className='w-full' src="/images/image.jpeg" width={240} height={240} alt="Capa do álbum" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-400'>Wallows, Coin, girl in red and more</span>
+           </a>
+           <a href='' className='bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2'>
+            <Image className='w-full' src="/images/image.jpeg" width={240} height={240} alt="Capa do álbum" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-400'>Wallows, Coin, girl in red and more</span>
+           </a>
+           <a href='' className='bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2'>
+            <Image className='w-full' src="/images/image.jpeg" width={240} height={240} alt="Capa do álbum" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-400'>Wallows, Coin, girl in red and more</span>
+           </a>
+           <a href='' className='bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2'>
+            <Image className='w-full' src="/images/image.jpeg" width={240} height={240} alt="Capa do álbum" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-400'>Wallows, Coin, girl in red and more</span>
+           </a>
+           <a href='' className='bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2'>
+            <Image className='w-full' src="/images/image.jpeg" width={240} height={240} alt="Capa do álbum" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-400'>Wallows, Coin, girl in red and more</span>
+           </a>
          </div>
        </main>
      </div>
-     <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-       footer
+     <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+       <div className='flex items-center gap-3'>
+        <Image className='w-fit' src="/images/image.jpeg" width={56} height={56} alt="Capa do álbum" />
+        <div className='flex flex-col'>
+          <strong className='font-normal'>Rope</strong>
+          <span className='text-xs text-zinc-400'>Foo Fighteres</span>
+        </div>
+       </div>
+       <div className='flex flex-col items-center gap-2'>
+        <div className='flex items-center gap-6'>
+          <Shuffle size={20} className='text-zinc-200' />
+          <SkipBack size={20} className='text-zinc-200' />
+          <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
+            <Play />
+          </button>
+          <SkipForward size={20} className='text-zinc-200' />
+          <Repeat size={20} className='text-zinc-200' />
+        </div>
+        <div className='flex items-center gap-2'>
+          <span className='text-sx text-zinc-400'>0:31</span>
+          <div className='h-1 rounded-full w-96 bg-zinc-600'>
+            <div className='bg-zinc-200 w-40 h-1 rounded-full'></div>
+          </div>
+          <span className='text-sx text-zinc-400'>2:14</span>
+        </div>
+       </div>
+       <div></div>
      </footer>
    </div>
   )
